862bfa8225ceadcdbb9ffb408e0761de02591f83
Cristina <cris_rsan@hotmail.com>
2014-05-08 18:48:43 +0200

    8maig-runtime

--- runtime/classify_region.m
+++ runtime/classify_region.m
@@ -1,10 +1,15 @@
 function [ped] = classify_region( row, col, size, path_to_image, image)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
+disp('Classifying region... r c s');
+row
+col
+size
 path_rid = '/nobackup/server/users/criru691/Dataset/INRIA/rid/';
 img2rid(image, path_to_image, path_rid); 
 region = zeros(1,3);
 fv=54;
+
 f = fopen('/nobackup/server/users/criru691/HOG+SVM/runtime/classifiers/svm_classifier.txt', 'r');
 ped = 0;
 res = 0;
@@ -12,11 +17,13 @@ neg = 0;
 
 region(1,1) = str2double(fscanf(f,'%s', 1));
 
-    while (~feof(f) || neg==1)
+    while (~feof(f) && neg==0)
        
         region(1,2) = str2double(fscanf(f,'%s', 1));
         region(1,3) = str2double(fscanf(f,'%s', 1));
         total_samples= str2double(fscanf(f,'%s', 1));
+       
+        HOG = zeros(total_samples, fv);
         for m=1:total_samples
           for n=1:fv
                 HOG(m,n) = str2double(fscanf(f, '%s', 1));
@@ -34,17 +41,24 @@ region(1,1) = str2double(fscanf(f,'%s', 1));
         path_to_rid_image = strcat(path_rid, image,'.rid');
         myCommand = ['./goh_extractor ' path_to_rid_image ' ' int2str(r) ' ' int2str(c) ' ' int2str(s)];
          
-        [status, res] = system(myCommand);
-        HOG = str2num(res);      
+        [status, hog] = system(myCommand);
+        HOG = str2num(hog);      
   
         weak_res = (svmclassify (structSVM.weak_svm, HOG))*a; %!!!!!!! ojuuuuuu la T varia per cada SVM - només actual ?
+        weak_res
+      
         res = res + weak_res;
-        
+        res
         aux = str2double(fscanf(f, '%s', 1));
         if(aux==999999)
+            disp('END OF STAGE');
             t = str2double(fscanf(f,'%s', 1));
-            region(1,1) = str2double(fscanf(f,'%s', 1));
-            if (res < t) neg=1; 
+            t
+            if (res < t) 
+                
+                neg=1; 
+            else
+                region(1,1) = str2double(fscanf(f,'%s', 1));
             end
         else 
             region(1,1) = aux;
@@ -52,8 +66,12 @@ region(1,1) = str2double(fscanf(f,'%s', 1));
         
     end
      
-    if(neg == 0 ) ped =1;
+    if(neg == 0 ) 
+        
+        ped = 1;
     end
+    neg
+    ped
     fclose(f);
 end
 
--- runtime/runtime.m
+++ runtime/runtime.m
@@ -11,7 +11,7 @@ im = imfinfo(path_to_image);
 minsize = 300; %minimum window size
 maxsize = im.Height; %maximum window size
 
-scale_factor=1.40;
+scale_factor=1.30;
 s=minsize;
 % WINDOS = 64x128.
 % IMAGE SCANNING - window selection
@@ -19,9 +19,9 @@ while(s <=maxsize)
     
     % HACER LOS OVERLAPS EXACTOS!!!!!!!!!!!!!
     %overlap_h=im.Width/(s*0.4);
-    overlap_h=100;
+    overlap_h=50;
     %overlap_v=im.Height/s;
-    overlap_v=100;
+    overlap_v=50;
     for r=0:overlap_v:(im.Height-s)
     %for r=round(s/2):8:round(im.Height-(s/2))
         for c=0:overlap_h:(im.Width-(s*0.4))
@@ -34,18 +34,17 @@ while(s <=maxsize)
             %pause()
             %ped = classify_region(r, c, s, path_to_image, image_name);
             ped = classify_region(r+round(s/2), c+((s*0.4)/2), s, path_to_image, image_name);
-            ped=0;
+            
             if (ped ==1)
                 %PEDESTRIAN DETECTED!
+                disp('Pedestrian detected!');
                 imshow(path_to_image);
                 rectangle('Position',[c, r, s*0.4, s], 'LineWidth', 2, 'EdgeColor', 'b');
                 pause()
             
             else
                 disp('NO pedestrians detected in this region - r -c');
-                r
-                c
-                s
+              
             end
         end
     end
